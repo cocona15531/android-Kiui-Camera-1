185a660fe373de8b8243f7855600f41e0b7476f8
Isseiissei <66742715+Isseiissei@users.noreply.github.com>
2021-02-09 14:17:37 +0900

    Initial Commit
    practice

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
+  File _image;
+
+  Future getImage() async{
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: implement build
+    return new MaterialApp(title: 'Image Picker',
+    home: new Scaffold(
+      appBar: new AppBar(title: new Text('Image Picker'),
+      ),
+      body: new Center(
+        child: _image == null? new Text('No image selected'):new Image.file(_image),
+      ),
+      floatingActionButton: new FloatingActionButton(onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: new Icon(Icons.camera),),
+    ),);
+  }
+}
